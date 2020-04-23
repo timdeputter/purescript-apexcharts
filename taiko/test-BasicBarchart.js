bd46fa8381a00431f33640a2b6bac31d6cd8f9b7
Tim de Putter <tim.de.putter83@googlemail.com>
2020-04-23 07:43:59 +0200

    rename test

--- taiko/test-BasicBarchart.js
+++ taiko/test-BasicBarchart.js
@@ -3,7 +3,7 @@ const { openBrowser, goto, text, $, closeBrowser, below } = require('taiko');
     try {
         console.log(`Current directory: ${process.cwd()}`);
         await openBrowser();
-        await goto("file:///" + process.cwd() + "/examples/BasicChart.html");
+        await goto("file:///" + process.cwd() + "/examples/BasicBarchart.html");
         
         [1991,1992,1993,1994,1995,1996,1997,1998,1999].forEach(async el => {
             await text(el.toString(), below($('text.apexcharts-text > tspan'))).exists()
