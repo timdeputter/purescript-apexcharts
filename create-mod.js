cc16c7ea37c9b99bf6f1cea80558b175f937d609
Tim de Putter <tim.de.putter83@googlemail.com>
2020-04-27 16:19:33 +0200

    script for creating modules from js-examples

---
+++ create-mod.js
@@ -0,0 +1,136 @@
+const fs = require('fs');
+
+
+
+var desc = {  legend: {
+    show: true,
+    showForSingleSeries: false,
+    showForNullSeries: true,
+    showForZeroSeries: true,
+    position: 'bottom',
+    horizontalAlign: 'center', 
+    floating: false,
+    fontSize: '14px',
+    fontFamily: 'Helvetica, Arial',
+    fontWeight: 400,
+    formatter: undefined,
+    inverseOrder: false,
+    width: undefined,
+    height: undefined,
+    tooltipHoverFormatter: undefined,
+    offsetX: 0,
+    offsetY: 0,
+    labels: {
+        colors: undefined,
+        useSeriesColors: false
+    },
+    markers: {
+        width: 12,
+        height: 12,
+        strokeWidth: 0,
+        strokeColor: '#fff',
+        fillColors: undefined,
+        radius: 12,
+        customHTML: undefined,
+        onClick: undefined,
+        offsetX: 0,
+        offsetY: 0
+    },
+    itemMargin: {
+        horizontal: 5,
+        vertical: 0
+    },
+    onItemClick: {
+        toggleDataSeries: true
+    },
+    onItemHover: {
+        highlightDataSeries: true
+    },
+}};
+
+
+var createTypeDefinitions = function(toCheck, currentName, parents, arr) {
+    arr.push({
+        filename: getFilename(parents, currentName), 
+        code: createFunctions(toCheck, currentName, parents, arr, 
+            createModuleAndDataDef(currentName, parents))
+    });
+};
+
+var getFilename = function(parents, currentName) {
+    return "./src/" + parents.filter(p => p !== "Apexoptions").join("") + capitalizeFLetter(currentName) + ".purs";
+};
+
+
+var createFunctions = function(toCheck, currentName, parents, arr, sourceCode) {
+    var names = Object.getOwnPropertyNames(toCheck);
+    names.forEach(name => {
+        var val = toCheck[name];
+        var type = typeof(val);
+        if(type === 'object'){
+            console.log(parents);
+            createTypeDefinitions(val, name, parents.concat([capitalizeFLetter(currentName)]), arr);
+        } else {
+            sourceCode += createFn(name, capitalizeFLetter(currentName), capitalizeFLetter(type));
+        }
+    });
+    return sourceCode;
+};
+
+
+var createModuleAndDataDef = function(currentName, parents) {
+    parents = parents.filter(p => p !== "Apexoptions")
+    if(parents.length > 0) {
+        var parentModuleImport = parents[0] == "Apexoptions" ? "import Apexcharts" : "import Apexcharts." + joinStrs(parents);
+        var moduleName = "Apexcharts." + joinStrs(parents) + "." + capitalizeFLetter(currentName); 
+        var sourceCode = `
+module ${moduleName} where
+
+${parentModuleImport}
+import Data.Functor.Contravariant (cmap)
+import Data.Options (Option, Options, opt)
+import Data.Options as Opt
+
+`;
+        sourceCode += createDataType(currentName, parents);
+        return sourceCode;
+    }
+    return "";
+};
+
+var createDataType = function(currentName, parents) {
+    var sourceCode = 'data ' + capitalizeFLetter(currentName) + "\n\n";
+    sourceCode += currentName + ' :: Option ' + capitalizeFLetter(parents[0]) + ' (Options ' + capitalizeFLetter(currentName) + ')\n';
+    sourceCode += currentName + ' = cmap Opt.options (opt \"' + currentName + '\")\n\n';
+    return sourceCode;    
+};
+
+var createFn = function(name, typeName, optTypeName) {
+    var res = "";
+    res += name + ' :: Option ' + typeName + ' ' + optTypeName + '\n';
+    res += name + ' = opt \"' + name + "\"\n\n";
+    return res;
+}
+
+var capitalizeFLetter = function(str) { 
+    return str[0].toUpperCase() +  str.slice(1); 
+};
+
+var joinStrs = function(arr) {
+    var res = "";
+    arr.forEach(element => {
+        res += element + ".";
+    });
+    return res.substring(0, res.length - 1);
+}
+
+var res = [];
+createTypeDefinitions(desc, 'Apexoptions', [], res)
+res.forEach(sc => {
+    console.log("Schreibe Datei: " + sc.filename);
+    fs.writeFile(sc.filename, sc.code);
+
+    console.log("done");
+});
+
+
